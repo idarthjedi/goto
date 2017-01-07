8cab4e0cc6ea8ec568424c486619c2a4880d308c
Jediah Logiodice <jediah@logiodice.com>
2017-01-07 16:21:20 -0500

    continue to build the foundation

---
+++ gotoserver.js
@@ -0,0 +1,18 @@
+var express = require('express'),
+ config = require('./server/configure'),
+app = express();
+
+app.set('port', process.env.PORT || 3300);
+app.set('views', __dirname + '/views');
+app = config(app);
+
+/*app.get('*', function(req, res){
+    res.send(req.url);
+    res.end();
+});*/
+
+
+app.listen(app.get('port'), function() {
+    console.log('Server up: http://localhost:' + app.get('port'));
+});
+
