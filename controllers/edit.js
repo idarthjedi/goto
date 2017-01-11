63772b45ee9e874f0523423c03d1fcbeffd37c7d
Jediah Logiodice <jediah@logiodice.com>
2017-01-10 21:55:30 -0500

    Started to connect the view & controllers for the edit functionality.  Also, implemented the route workflow as defined in the diagram 01 - Link Flow.

--- controllers/edit.js
+++ controllers/edit.js
@@ -1,5 +1,45 @@
 module.exports = {
     index: function(req, res) {
-        res.render('edit');
+
+        var viewModel = {
+            urls: [
+                {
+                    uniqueId: 1,
+                    name: 'jediah',
+                    url_id: 'http://darthjedi.logiodice.com/',
+                    description: 'This is Jediah\'s Blog website',
+                    views: 100,
+                    lastmodified: Date.now
+                },
+                {
+                    uniqueId: 2,
+                    name: 'donovan',
+                    url_id: 'http://donovan.logiodice.com/',
+                    description: 'This is Donovan\'s website',
+                    views: 2,
+                    lastmodified: Date.now
+                },
+                {
+                    uniqueId: 3,
+                    name: 'braeden',
+                    url_id: 'http://braeden.logiodice.com/',
+                    description: "This is Braeden\'s website",
+                    views: 4,
+                    lastmodified: Date.now
+                }
+            ]
+        };
+
+                res.render('edit', viewModel);
+    },
+    create: function(req, res) {
+        res.send('The image:create POST controller');
+    },
+    like: function(req, res) {
+        res.send('The image:like POST controller');
+    },
+    comment: function(req, res) {
+        res.send('The image:comment POST controller');
     }
+
 };
--- server/routes.js
+++ server/routes.js
@@ -1,15 +1,15 @@
 var express = require('express'),
     router = express.Router(),
     edit = require('../controllers/edit'),
-    image = require('../controllers/image');
+    //image = require('../controllers/image');
     bounce = require('../controllers/bounce');
 
 module.exports = function(app) {
-    router.get('/images/:image_id', image.index);
-    router.post('/images', image.create);
-    router.post('/images/:image_id/like', image.like);
-    router.post('/images/:image_id/comment', image.comment);
+    router.get('/edit/:url_id', edit.index);
     router.get('/edit', edit.index);
+    router.post('/edit', edit.create);
+    router.post('/edit/:url_id/like', edit.like);
+    router.post('/edit/:url_id/comment', edit.comment);
     router.get('*', bounce.index);
     app.use(router);
 };
